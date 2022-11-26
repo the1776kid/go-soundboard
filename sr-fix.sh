0c6d056ff2fbcb06f542b292015a21f770b7b3f7
the1776kid <the1776kid@gmail.com>
2022-11-26 12:52:09 -0500

    sample rate script update

--- sr-fix.sh
+++ sr-fix.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 targetSampleRate=48000
-for i in $(ls audio/) ; do
-  sr=$(ffprobe -hide_banner -loglevel error -show_entries stream=sample_rate audio/${i} | awk 'FNR == 2 {print}')
-  echo "audio/${i} ${sr}"
+audioDir=$1
+# TODO is $1 is nil print help
+echo "${audioDir}"
+for i in $(ls ${audioDir}) ; do
+  sr=$(ffprobe -hide_banner -loglevel error -show_entries stream=sample_rate ${audioDir}${i} | awk 'FNR == 2 {print}')
+  echo "${audioDir}${i} ${sr}"
   if [[ ${sr} != *"${targetSampleRate}"* ]];then
     echo "transcode ${i}, set sample rate ${targetSampleRate}hz"
-    mv audio/${i} audio/tempFile.mp3
-    ffmpeg -i audio/tempFile.mp3 -hide_banner -loglevel error -af aresample=resampler=soxr -ar ${targetSampleRate} audio/${i}
-    rm audio/tempFile.mp3
+    mv ${audioDir}${i} ${audioDir}tempFile.mp3
+    ffmpeg -i ${audioDir}tempFile.mp3 -hide_banner -loglevel error -af aresample=resampler=soxr -ar ${targetSampleRate} ${audioDir}${i}
+    rm ${audioDir}tempFile.mp3
   fi
 done
