8c61ea7a2fe6f9b78f3d8504b07fde3e44d77ef6
the1776kid <the1776kid@gmail.com>
2022-11-25 22:52:57 -0500

    concurrent playback, fixed most data races

--- main.go
+++ main.go
@@ -13,26 +13,30 @@ import (
 	"strings"
 )
 
-type soundboard struct {
+var (
 	otoContext *oto.Context
-	ap         *oto.Player
 	content    map[string][]byte
-}
+)
 
-func (s *soundboard) play(input []byte) {
-	if _, err := s.ap.Write(input); err != nil {
-		log.Panicf("%v", err)
+func play(input []byte) {
+	ap := otoContext.NewPlayer()
+	if _, err := ap.Write(input); err != nil {
+		log.Panicf("failed writing to player: %v", err)
+	}
+	if err := ap.Close(); err != nil {
+		log.Panicf("failed to clone player: %v", err)
 	}
 }
 
-func (s *soundboard) gui() {
+func gui() {
 	a := app.New()
 	w := a.NewWindow("soundboard")
 	vb := container.NewVBox()
-	for s2, bytes := range s.content {
+	for index, bytes := range content {
+		sl := index
 		nb := bytes
-		vb.Add(widget.NewButton(s2, func() {
-			s.play(nb)
+		vb.Add(widget.NewButton(sl, func() {
+			go play(nb)
 		}))
 	}
 	w.SetContent(vb)
@@ -40,13 +44,11 @@ func (s *soundboard) gui() {
 }
 
 func main() {
-	s := soundboard{}
 	var otoErr error
-	s.otoContext, otoErr = oto.NewContext(48000, 2, 2, 256)
+	otoContext, otoErr = oto.NewContext(48000, 2, 2, 256)
 	if otoErr != nil {
 		log.Panicf("Error creating oto.NewContext %v", otoErr)
 	}
-	s.ap = s.otoContext.NewPlayer()
 	var path string
 	flag.StringVar(&path, "d", "audio/", "dir of 48khz mp3 files")
 	flag.Parse()
@@ -54,7 +56,7 @@ func main() {
 	if err != nil {
 		return
 	}
-	s.content = map[string][]byte{}
+	content = map[string][]byte{}
 	for _, entry := range dir {
 		en := entry.Name()
 		if en[len(en)-4:] != ".mp3" {
@@ -68,9 +70,9 @@ func main() {
 		if err != nil {
 			log.Panicf("Error decoding file %s: %v", entry.Name(), err)
 		}
-		if s.content[strings.Replace(en, ".mp3", "", 1)], err = io.ReadAll(decodedFile); err != nil {
+		if content[strings.Replace(en, ".mp3", "", 1)], err = io.ReadAll(decodedFile); err != nil {
 			log.Panicf("Error reading decodedFile %s: %v", entry.Name(), err)
 		}
 	}
-	s.gui()
+	gui()
 }
