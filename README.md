291273bc23fcd5b9e07d945ed17b286801dc437f
the1776kid <the1776kid@gmail.com>
2022-11-26 14:45:30 -0500

    readme, windows build disclaimer, more test files

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ---
 
 WIP
+
+
+## Windows
+your on your own, windows is trash when it comes to setting up dev env(windows should be shipped with GCC). i use linux you should too.
--- sr-fix.sh
+++ sr-fix.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 targetSampleRate=48000
 audioDir=$1
-# TODO is $1 is nil print help
+# TODO if $1 is nil print help
 echo "${audioDir}"
 for i in $(ls ${audioDir}) ; do
   sr=$(ffprobe -hide_banner -loglevel error -show_entries stream=sample_rate ${audioDir}${i} | awk 'FNR == 2 {print}')
