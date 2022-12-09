c6039ecc99fd80906afbca517c230567efe0010b
the1776kid <the1776kid@gmail.com>
2022-12-09 12:02:05 -0500

    script update

--- sr-fix.sh
+++ sr-fix.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+#targetSampleRate=44100
 targetSampleRate=48000
 audioDir=$1
 # TODO if $1 is nil print help
 echo "${audioDir}"
 for i in $(ls ${audioDir}) ; do
+  if [[ $i != *.mp3 ]] ; then
+      echo "${i} is not mp3, skipping"
+      continue
+  fi
   sr=$(ffprobe -hide_banner -loglevel error -show_entries stream=sample_rate ${audioDir}${i} | awk 'FNR == 2 {print}')
   echo "${audioDir}${i} ${sr}"
   if [[ ${sr} != *"${targetSampleRate}"* ]];then
